5c6c4bd6f69fe63114bdeae39495c8a71a6e95f3
AB498 <abcd49800@gmail.com>
2023-08-03 13:10:09 +0600

    dasdas

--- test.cpp
+++ test.cpp
@@ -1,95 +1,96 @@
-#include <sstream>
+#include <iostream>
 #include <string>
-#include <vector>
-#include <map>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-
-template <typename T>
-std::string ConvToString(const T &value)
-{
-    std::cout << "_res_";
-    std::ostringstream os;
-    os << value;
-    return os.str();
-}
-
-template <typename T>
-std::string ConvToString(const std::vector<T> &vec)
-{
-    std::ostringstream os;
-    os << "[";
-    for (size_t i = 0; i < vec.size(); ++i)
-    {
-        if (i > 0)
-            os << ", ";
-        os << ConvToString(vec[i]);
-    }
-    os << "]";
-    return os.str();
-}
-
-template <typename K, typename V>
-std::string ConvToString(const std::map<K, V> &map)
-{
-    std::ostringstream os;
-    os << "{";
-    for (auto it = map.begin(); it != map.end(); ++it)
-    {
-        if (it != map.begin())
-            os << ", ";
-        os << ConvToString(it->first) << ": " << ConvToString(it->second);
-    }
-    os << "}";
-    return os.str();
-}
-
-template <typename T>
-std::string ConvToString(const T *arr, size_t N)
-{
-    std::ostringstream os;
-    os << "[";
-    for (size_t i = 0; i < N; ++i)
-    {
-        if (i > 0)
-            os << ", ";
-        os << ConvToString(arr[i]);
-    }
-    os << "]";
-    return os.str();
-}
-
-template <typename T, size_t N>
-std::string ConvToString(const T (&arr)[N])
-{
-    std::ostringstream os;
-    os << "[";
-    for (size_t i = 0; i < N; i++)
-    {
-        os << ConvToString(arr[i]);
-        if (i != N - 1)
-            os << ", ";
-    }
-    os << "]";
-    return os.str();
-}
-std::string ConvToString(const char *value)
-{
-    return std::string(value);
-}
-
-#include <iostream>
 using namespace std;
-int hello()
+class Course
 {
-    return 1;
-}
+    string name;
+    int credits;
+    int creditHours;
+
+public:
+    int multiplier;
+    Course()
+    {
+        multiplier = 5;
+    }
+    Course(int creds)
+    {
+        multiplier = 5;
+        credits = creds;
+    }
+    int getCreditHours()
+    {
+        return credits * multiplier;
+    }
+    string getName()
+    {
+        return name;
+    }
+    void setName(string n)
+    {
+        name = n;
+    }
+    void setMultiplier(int m)
+    {
+        multiplier = m;
+    }
+    void setCredits(int c)
+    {
+        credits = c;
+    }
+    int getCredits()
+    {
+        return credits;
+    }
+    Course operator+(Course c)
+    {
+        return Course(c.getCredits() + getCredits());
+    }
+};
+class Mandatory
+{
+public:
+    int isMandatory = 1;
+};
+class Optional
+{
+public:
+    int isMandatory = 0;
+};
+// single inheritance
+class CreditlessCourse : public Course
+{
+public:
+    CreditlessCourse(int c)
+    {
+        setMultiplier(0);
+        setCredits(c);
+    }
+};
+// single inheritance
+class BaseCourse : public Course
+{
+public:
+    BaseCourse(int c)
+    {
+        setCredits(c);
+    }
+};
+// multiple inheritance: Mandatory, BaseCourse > ENG_Course
+// multi-level inheritance: Course > BaseCourse > ENG_Course
+class ENG_Course : public Mandatory, public BaseCourse
+{
+public:
+    ENG_Course(string n) : BaseCourse(10)
+    {
+        setName(n);
+    }
+};
+
 int main()
 {
-    there
-    hello();
-    int a = 4 - ConvToString(hello());
-    cout << "Hello World!";
+    ENG_Course en = ENG_Course("ENG-1100");
+    cout << en.getName() << endl;
+    cout << (en + en + en).getCreditHours() << endl;
     return 0;
 }
--- wasmclang/app.js
+++
@@ -1,5 +0,0 @@
-const app = require('express')();
-
-app.use("/", require("express").static("./"));
-
-app.listen(3000, () => console.log('Listening on http://localhost:3000/'));
