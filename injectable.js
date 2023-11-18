61bac7def3b0c204494bf9be3b5dad92dee3da96
zubaer-ahmed <zubaerahmed100@gmail.com>
2023-11-18 23:59:14 +0600

    a

--- injectable.js
+++ injectable.js
@@ -1,13 +1,13 @@
-var capturedLogs = [];
-var printedLogs = [];
+window.capturedLogs = [];
+window.printedLogs = [];
 
-var originalConsoleLog = console.log;
-_special_debug = function (line, col, arg) {
+window.originalConsoleLog = console.log;
+window._special_debug = function (line, col, arg) {
     capturedLogs.push([line, col, arg]);
     return arg;
 };
-_special_console_log = function (line, col, ...args) {
+window._special_console_log = function (line, col, ...args) {
     capturedLogs.push([line, col, args.length == 1 ? args[0] : args]);
-    printedLogs.push([line, col, args.join(' ')]);
+    printedLogs.push([line, col, args.length == 1 ? args[0] : args]);
     // console.log(...args);
 };
---
+++ test.js
@@ -0,0 +1,19 @@
+try {
+    throw new Error('foo');
+} catch (e) {
+    console.log(e.message);
+    console.log(e.stack);
+
+    const errorStack = `
+        ReferenceError: d is not defined\n
+        at eval (eval at runCurrentFile (http://127.0.0.1:3000/hotcode/js.html:159:34), <anonymous>:8:2)\n
+        at eval (<anonymous>)\n
+        at runCurrentFile (http://127.0.0.1:3000/hotcode/js.html:614:34)\n
+        at http://127.0.0.1:3000/hotcode/js.html:540:13"
+    `;
+    errorLogs = {};
+    errorLogs.stack = errorStack;
+    const [lineno, colno] = errorLogs.stack.match(/<anonymous>.*?(\d+:\d+)\)\n/)[1].split(':').map(Number);
+    console.log('Line:', lineno);
+    console.log('Column:', colno);
+}
